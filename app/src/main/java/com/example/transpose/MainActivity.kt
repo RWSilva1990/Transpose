4bfddca049a1214debb4d9a4b12e4b8f6b145731
joh9911 <joh9911@naver.com>
2025-04-15 20:44:00 +0900

    Update MainActivity

--- app/src/main/java/com/example/transpose/MainActivity.kt
+++ app/src/main/java/com/example/transpose/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.transpose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.main.MainScreen
+import com.example.ui.theme.TransposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 
