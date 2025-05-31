f4660a9e874819fc88443189aefb0b6088a405c1
joh9911 <joh9911@naver.com>
2025-05-31 14:50:24 +0900

    refactor: move myPlaylist state to VideoRepository

--- core/domain/src/main/java/com/example/domain/repository/MyPlaylistDBRepository.kt
+++ core/domain/src/main/java/com/example/domain/repository/MyPlaylistDBRepository.kt
@@ -3,14 +3,17 @@ package com.example.domain.repository
 import com.example.domain.model.library.MyPlaylist
 import com.example.domain.model.youtube.video.Video
 import com.example.domain.model.youtube.video_detail.VideoDetail
+import kotlinx.coroutines.flow.StateFlow
 
 
 interface MyPlaylistDBRepository {
-    suspend fun createPlaylist(name: String): Long
-    suspend fun getAllPlaylists(): List<MyPlaylist>
+    val myPlaylists: StateFlow<List<MyPlaylist>>
+    val myPlaylistItems: StateFlow<List<Video>>
+    suspend fun createPlaylist(name: String)
+    suspend fun getAllPlaylists()
     suspend fun deletePlaylist(playlistId: Long)
     suspend fun addVideoToPlaylist(video: Video, playlistId: Long)
     suspend fun addVideoToPlaylist(video: VideoDetail, playlistId: Long)
-    suspend fun getVideosForPlaylist(playlistId: Long): List<Video>
+    suspend fun getVideosForPlaylist(playlistId: Long)
     suspend fun deleteVideoFromPlaylist(playlistId: Long, video: Video)
 }
