050400a9499c0777145e828862b8bf6fdf6fd20a
joh9911 <joh9911@naver.com>
2025-06-15 13:40:55 +0900

    feat: implement deferring state read + use lambda to change the offset in Layout time

--- feature/main/src/main/java/com/example/main/components/bottom_navigation/BottomNavigationBar.kt
+++ feature/main/src/main/java/com/example/main/components/bottom_navigation/BottomNavigationBar.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.main.components.appbar.SearchBarState
@@ -23,33 +24,44 @@ import com.example.util.constants.AppColors
 @Composable
 fun BottomNavigationBar(
     searchBarState: SearchBarState,
-    bottomSheetOffset: Float,
+    bottomSheetOffset: () -> Float,
     selectedTab: MainTab,
     onTabSelected: (MainTab) -> Unit,
 ) {
 
-    val totalOffset = remember(bottomSheetOffset, searchBarState) {
-        derivedStateOf {
-            // BottomSheet에 의한 오프셋 계산
-            val normalizedBottomSheetOffset = when {
-                bottomSheetOffset <= 0f -> 0.dp
-                bottomSheetOffset >= 1f -> 56.dp
-                else -> (bottomSheetOffset * 56).dp
-            }
-
-            // SearchBar 상태에 의한 오프셋
-            val searchBarOffset = if (searchBarState == SearchBarState.OPENED) 56.dp else 0.dp
-
-            normalizedBottomSheetOffset + searchBarOffset
-        }
-    }
+//    val totalOffset = remember(bottomSheetOffset(), searchBarState) {
+//        derivedStateOf {
+//            // BottomSheet에 의한 오프셋 계산
+//            val normalizedBottomSheetOffset = when {
+//                bottomSheetOffset() <= 0f -> 0.dp
+//                bottomSheetOffset() >= 1f -> 56.dp
+//                else -> (bottomSheetOffset() * 56).dp
+//            }
+//
+//            // SearchBar 상태에 의한 오프셋
+//            val searchBarOffset = if (searchBarState == SearchBarState.OPENED) 56.dp else 0.dp
+//
+//            normalizedBottomSheetOffset + searchBarOffset
+//        }
+//    }
 
     val mainTabs = remember { MainTab.ALL_TABS }
 
     BottomNavigation(
         modifier = Modifier
             .navigationBarsPadding()
-            .offset(y = totalOffset.value),
+            .offset{
+                val normalizedBottomSheetOffset = when {
+                    bottomSheetOffset() <= 0f -> 0.dp
+                    bottomSheetOffset() >= 1f -> 56.dp
+                    else -> (bottomSheetOffset() * 56).dp
+                }
+
+                val searchBarOffset = if (searchBarState == SearchBarState.OPENED) 56.dp else 0.dp
+                val totalOffset = normalizedBottomSheetOffset + searchBarOffset
+
+                IntOffset(0, totalOffset.roundToPx())
+            },
         backgroundColor = AppColors.BlueBackground
 
     ) {
