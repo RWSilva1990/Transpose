12546b29eca1e98d011cb84e7b3bb510d2b07f30
joh9911 <joh9911@naver.com>
2025-08-04 02:54:22 +0900

    feat: add video quality selection UI and update media item based on selection

--- build-logic/convention/src/main/java/com/example/convention/MediaConvention.kt
+++ build-logic/convention/src/main/java/com/example/convention/MediaConvention.kt
@@ -13,5 +13,7 @@ internal fun Project.configureMedia(
         add("implementation", libs.findLibrary("androidx.media3.exoplayer.dash").get())
         add("implementation", libs.findLibrary("androidx.media3.exoplayer.hls").get())
         add("implementation", libs.findLibrary("androidx.media3.ui").get())
+        add("implementation", libs.findLibrary("androidx.media3.datasource").get())
+
     }
 }
